a84d4880f93be92a07ad7137620ebfaca77b22c0
garam <garam08@bk.ru>
2025-01-19 11:14:53 +0300

    Добавление функции перевод юани в рубли и обратно

--- get_yani.py
+++ get_yani.py
@@ -1,25 +1,51 @@
 from pycbrf.toolbox import ExchangeRates
 import datetime
+from math import ceil
+import requests
+from config import COURSE_TOKEN
+from bs4 import BeautifulSoup
 
 
-def get_course():
+def get_course_cny(n):
     current_date = str(datetime.date.today().isoformat())
     rates = ExchangeRates(current_date)  # задаем дату, за которую хотим получить данные
     result = rates['CNY']
-    print(result.value)
     '''
-    Внутри объекта result будут храниться следующие данные:
-    ExchangeRate(
-                id='R01235',
-                name='Доллар США',
-                code='USD',
-                num='840',
-                value=Decimal('65.5287'),
-                par=Decimal('1'),
-                rate=Decimal('65.5287'))
-    Можем обратиться к ним так:
-    result.name # вернет Доллар США
-    '''
+        Внутри объекта result будут храниться следующие данные:
+        ExchangeRate(
+                    id='R01235',
+                    name='Доллар США',
+                    code='USD',
+                    num='840',
+                    value=Decimal('65.5287'),
+                    par=Decimal('1'),
+                    rate=Decimal('65.5287'))
+        Можем обратиться к ним так:
+        result.name # вернет Доллар США
+        '''
+    return int(ceil(n / result.value))
+
+
+def get_exchange_rate():
+    url = "https://api.exchangerate-api.com/v4/latest/RUB"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['rates']['CNY']  # Получаем курс юаня
+    else:
+        print("Ошибка при получении данных:", response.status_code)
+        return None
+
+
+def get_course_rub(n):
+    exchange_rate = get_exchange_rate()
+    if exchange_rate is not None:
+        amount_cny = n * exchange_rate
+        return ceil(amount_cny)
+    else:
+        return None
+
 
 if __name__ == "__main__":
-    get_course()
+    pass
--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 import logging
 from aiogram import Bot, Dispatcher, types, F
-from aiogram.filters.command import Command
-import asyncio
+from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
 from config import TOKEN
-from get_yani import get_course
-
+import asyncio
+from get_yani import get_course_cny, get_course_rub
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+
+class Uyan(StatesGroup):
+    calculate_price = State()
+
+
+class Ruble(StatesGroup):
+    calculate_price = State()
+
+
 KEYBOARD_START = [
     [types.KeyboardButton(text="ОФОРМИТЬ ЗАКАЗ")],
-    [types.KeyboardButton(text="КАЛЬКУЛЯТОР СТОИМОСТИ")],
-    [types.KeyboardButton(text="ПОМОЩЬ")],
+    [types.KeyboardButton(text="ПЕРЕВЕСТИ ЮАНИ В РУБЛИ")],
+    [types.KeyboardButton(text="ПЕРЕВЕСТИ РУБЛИ В ЮАНИ")],
+    [types.KeyboardButton(text="ПОМОЩЬ")]
 ]
+
 
 @dp.message(Command('start'))
 async def start_message(message: types.Message):
@@ -26,17 +38,59 @@ async def start_message(message: types.Message):
     await message.answer('Добро пожаловать! Данный тг бот поможет вам оформить заказ с Poizon. Нажмите на '
                          'одну из кнопок ниже для продолжения работы:', reply_markup=keyboard)
 
+
 @dp.message(F.text.lower() == "помощь")
 async def help_message(message: types.Message):
     await message.answer('Здесь будет размещена помощь. Чтобы вернуться назад нажмите /start')
 
 
+@dp.message(F.text.lower() == "перевести юани в рубли")
+async def calculate_price_start(message: types.Message, state: FSMContext):
+    await message.answer('Введите цену товара в рублях, я переведу Вам в юани (чтобы начать сначала введите /start)')
+    await state.set_state(Uyan.calculate_price)
+
+
+@dp.message(Uyan.calculate_price)
+async def calculate_price(message: types.Message, state: FSMContext):
+    try:
+        price = int(message.text)
+        print(price)
+        new_price = get_course_cny(price)
+        print(new_price)
+        await message.answer(f"{price} RUB = {new_price} CNY")
+        await message.answer(f"Нажмите /start, чтобы вернуться обратно")
+        await state.clear()  # Очистить состояние после обработки
+    except ValueError:
+        await message.answer('Возникла ошибка, вероятно вы неправильно ввели данные. Вернитесь назад, нажав /start')
+
+
+@dp.message(F.text.lower() == "перевести рубли в юани")
+async def calculate_price_start(message: types.Message, state: FSMContext):
+    await message.answer('Введите цену товара в рублях, я переведу Вам в юани (чтобы начать сначала введите /start)')
+    await state.set_state(Ruble.calculate_price)
+
+
+@dp.message(Ruble.calculate_price)
+async def calculate_price(message: types.Message, state: FSMContext):
+    try:
+        price = int(message.text)
+        # print(price)
+        new_price = get_course_rub(price)
+        # print(new_price)
+        await message.answer(f"{price} RUB = {new_price} CNY")
+        await message.answer(f"Нажмите /start, чтобы вернуться обратно")
+        await state.clear()  # Очистить состояние после обработки
+    except ValueError:
+        await message.answer('Возникла ошибка, вероятно вы неправильно ввели данные. Вернитесь назад, нажав /start')
+
+
 async def main():
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     try:
         print("Bot's working has been started...")
         asyncio.run(main())
-    except:
-        print("Something is going wrong, please wait...")
+    except Exception as e:
+        print(f"Something is going wrong: {e}")
